710abb0c88390c2e6aae100f2e4e63e0b2b55031
Rachit <rachit2311@gmail.com>
2025-08-01 03:04:07 +0200

    removed unused file

--- app/deps.py
+++
@@ -1,9 +0,0 @@
-# app/deps.py
-from typing import AsyncGenerator
-from app.core.database import AsyncSessionLocal
-from app.services.user import UserService
-from app.repositories.user import SQLAlchemyUserRepository
-
-async def get_user_service() -> AsyncGenerator[UserService, None]:
-    async with AsyncSessionLocal() as session:
-        yield UserService(SQLAlchemyUserRepository(session))
